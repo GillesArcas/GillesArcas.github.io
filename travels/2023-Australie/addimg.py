073bffbc327f2d72ef514a6e952cd22d98e7158f
Gilles Arcas <gilles.arcas@gmail.com>
2023-04-11 20:45:33 +1000

    Add videos.

--- travels/2023-Australie/addimg.py
+++ travels/2023-Australie/addimg.py
@@ -11,7 +11,7 @@ INDEXMD = r"D:\Gilles\github.io\travels\2023-Australie\part1\index.md"
 
 def get_image_list():
     """
-    Get list of image basenames from clipboard
+    Get list of file basenames from clipboard
     """
     clip = clipboard.paste()
     imglist = list()
@@ -23,21 +23,41 @@ def get_image_list():
     return imglist
 
 
-def resample(imgin, imgout):
+def resample_photo(imgin, imgout):
     img = Image.open(imgin)
     new_img = img.resize((1280,960), Image.ANTIALIAS)
     quality = 90
     new_img.save(imgout, "JPEG", quality=quality)
 
 
+def resample_movie(movin, movout):
+    os.system(f'ffmpeg -i {movin} -vf scale=720:-2 -c:a copy {movout}')
+
+
+def resample(fnin, fnout):
+    if os.path.splitext(fnin)[1] == '.jpg':
+        resample_photo(fnin, fnout)
+    else:
+        resample_movie(fnin, fnout)
+
+
+def media_declaration(media):
+    if os.path.splitext(media)[1] == '.jpg':
+        return f'![]({media})\n'
+    else:
+        return f'[]({media})\n'
+
+
 def add_to_diary(imglist):
     with open(INDEXMD) as f:
         lines = f.readlines()
-        for index, line in enumerate(lines):
-            if line.startswith('![]('):
-                last = index
 
-    lines = lines[:last] + [f'![]({x})\n' for x in imglist] + lines[last + 1:]
+    # find last media in diary
+    for index, line in enumerate(lines):
+        if line.startswith('![](') or line.startswith('[]('):
+            last = index
+
+    lines = lines[:last] + [media_declaration(x) for x in imglist] + lines[last + 1:]
     with open(INDEXMD, 'wt') as f:
         f.writelines(lines)
 
